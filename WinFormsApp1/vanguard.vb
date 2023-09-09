ceb955971c04cc5d44ccec1951e03587a93146a5
rivrub <138538639+rivrub@users.noreply.github.com>
2023-09-09 23:56:48 +0100

    Delete WinFormsApp1 directory

--- WinFormsApp1/vanguard.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class VanguardChecker
-    Public Shared Function IsVanguardRunning() As Boolean
-        Dim processes() As Process = Process.GetProcessesByName("vgtray")
-        Return processes.Length > 0
-    End Function
-End Class
